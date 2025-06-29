b477a01b52eb3110c68fc132ac0f7100836d743d
VictorTaelin <victortaelin@gmail.com>
2025-06-29 10:29:48 -0300

    rename

--- Nat/add/comm.bend.py
+++
@@ -1,8 +0,0 @@
-def Nat/add/comm(a: Nat, b: Nat) -> Nat{Nat/add(a,b) == Nat/add(b,a)}:
-  match a:
-    case 0n:
-      Nat/add/zero_right(b)
-    case 1n+ap:
-      rewrite Nat/add/comm(ap,b)
-      rewrite Nat/add/succ_right(b,ap)
-      finally
--- main.bend.py
+++
@@ -1,5 +0,0 @@
-def main : Char =
-  U64_TO_CHAR(65)
-
-#def main : String =
-  #Bend/show(@Lam{"x", λx. @Lam{"y", λy. @App{x,y}}})
